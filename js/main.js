297f22d8dd1a9784f5ce8e9c162844e10481313c
Nazariy Teslya <nazariy.teslya@marconirovereto.it>
2025-01-15 12:52:10 +0100

    feat(html, js): parte sul menu

--- js/main.js
+++ js/main.js
@@ -1,9 +1,33 @@
 const lista = document.querySelector('#main-menu');
 
-//console.log(losta);
+
+const articolo = {
+    type: 'article',
+    children: [
+        {
+            type: 'h2',
+            children: 'article title'
+        },
+        {
+            type: 'p',
+            children: 'article content'
+        }
+    ]
+
+}
+const rootContent = document.querySelector('#content');
+createContent(article,);
+
 
 const item = document.createElement('li');
+item.classList.add('menu-item');
+item.style.color = 'red';
 
 item.innerHTML = '<a href="#">Contacts</a>';
+
+const link = item.querySelector('a');
+
+link.style.color = 'green'; 
+link.style.textDecoration = 'none'; 
 
 lista.appendChild(item);
