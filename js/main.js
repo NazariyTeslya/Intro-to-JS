38f7e2467303d085beda29a437494c65a2284cf0
Nazariy Teslya <nazariy.teslya@marconirovereto.it>
2025-01-15 12:28:55 +0100

    feat(js): aggiungi voce al menu

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const lista = document.querySelector('#main-menu');
+
+//console.log(losta);
+
+const item = document.createElement('li');
+
+item.innerHTML = '<a href="#">Contacts</a>';
+
+lista.appendChild(item);
